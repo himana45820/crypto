b8c9479ada85a7a4a8b093c8ba1bb2aa384d5811
Jagruit <jagruti@enlighteninfosystems>
2026-04-16 12:02:51 +0530

    fix run debug

--- app.py
+++ app.py
@@ -255,5 +255,10 @@ def predict():
         import traceback
         return jsonify({"error": str(e), "traceback": traceback.format_exc()}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5005)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5005)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
